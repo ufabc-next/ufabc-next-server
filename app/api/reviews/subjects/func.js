87159ffadb8671c1af9ee3d91fc12d1d11ecf59c
Felipe Santos <felipe.augusto@tenda.digital>
2019-03-21 18:06:19 -0300

    Improve reviews with more info

--- app/api/reviews/subjects/func.js
+++ app/api/reviews/subjects/func.js
@@ -9,14 +9,20 @@ module.exports = async function (context) {
     return
   }
 
+  const cacheKey = `reviews_${subjectId}`
+  let cached =  await app.redis.cache.get(cacheKey)
+
+  if(cached){
+    return cached
+  }
+
   subjectId = mongoose.Types.ObjectId(subjectId)
 
   let stats = await app.models.enrollments.aggregate([
     { 
-      $match: { 
-        subject: subjectId, 
-        cr_acumulado: { $exists: true }, 
-        conceito: { $exists: true } 
+      $match: {
+        subject: subjectId,
+        conceito: { $in: ['A', 'B', 'C', 'D', 'O', 'F'] } 
       } 
     },
     {
@@ -27,9 +33,44 @@ module.exports = async function (context) {
           subject: '$subject'
         },
         cr_medio: { $avg: "$cr_acumulado" },
-        count: { "$sum": 1 }
+        total: { "$sum": 1 },
+        crs: { $push: "$cr_acumulado" },
+        weight: {
+          $first : {
+            $switch: {
+             branches: [
+                { case: { $eq: ["$conceito", "A"]}, then: 4 },
+                { case: { $eq: ["$conceito", "B"]}, then: 3 },
+                { case: { $eq: ["$conceito", "C"]}, then: 2 },
+                { case: { $eq: ["$conceito", "D"]}, then: 1 },
+                { case: { $eq: ["$conceito", "O"]}, then: 0 },
+                { case: { $eq: ["$conceito", "F"]}, then: 0 }
+             ],
+             default: null
+            }
+          }
+        },
       }
-    }, 
+    }, {
+      $addFields: {
+        crs: {
+          $filter: {
+            input: "$crs",
+            as: "d",
+            cond: { $ne: [ "$$d", null ] }
+          }
+        }
+      }
+    }, {
+      $project: {
+        _id: 1,
+        cr_medio: 1,
+        total: 1,
+        weight: 1,
+        crs: 1,
+        amount: { $size: "$crs" },
+      }
+    },
     { 
       $group: {
         _id: {
@@ -38,30 +79,51 @@ module.exports = async function (context) {
         distribution: {
           $push: {
             conceito: "$_id.conceito",
-            count: "$count",
-            cr_medio: "$cr_medio"
+            weight: "$weight",
+            total: "$total",
+            cr_medio: "$cr_medio",
+            numeric: { $multiply: ["$amount", "$cr_medio"] },
+            numericWeight: { $multiply: ["$amount", "$weight"] },
+            amount: "$amount",
           }
-        },
-        cr_medio: { $avg: "$cr_medio" },
-        count: { $sum: "$count" },
+        }, 
+        numericWeight: { $sum : { $multiply: ["$amount", "$weight"] } },
+        numeric: { $sum : { $multiply: ["$amount", "$cr_medio"] } },
+        amount: { $sum: "$amount" },
+        total: { $sum: "$total" }
       }
     }, {
       $project: {
         distribution: 1,
-        cr_medio: 1, 
-        count: 1,
+        numericWeight: 1,
+        numeric: 1,
+        amount: 1,
+        total: 1, 
+        cr_professor: { $divide : ["$numericWeight", "$amount"]},
         teacher: "$_id.mainTeacher"
       }
     }
   ])
 
+  // calculate cr_medio
+  stats = stats.map(s => {
+    s.cr_medio = s.numeric / s.amount
+    return s
+  })
+
   function getMean(value, key) {
-    const totalCount = _.sumBy(value, 'count')
-    const simpleSum = value.map(v => v.count * v.cr_medio)
+    const total = _.sumBy(value, 'total')
+    const amount = _.sumBy(value, 'amount')
+    const simpleSum = value.filter(v => v.cr_medio != null).map(v => v.amount * v.cr_medio)
+
     return {
       conceito: key,
-      cr_medio: _.sum(simpleSum) / totalCount,
-      count: totalCount
+      cr_medio: _.sum(simpleSum) / amount,
+      cr_professor: _.sumBy(value, 'numericWeight') / amount,
+      total: total,
+      amount:  amount,
+      numeric: _.sumBy(value, 'numeric'),
+      numericWeight:  _.sumBy(value, 'numericWeight'),
     }
   }
 
@@ -73,9 +135,12 @@ module.exports = async function (context) {
     .values()
     .value()
 
-  return {
+  const resp = {
     subject: await app.models.subjects.findOne({ _id: subjectId }).lean(true),
     general: _.merge(getMean(generalDistribution), { distribution: generalDistribution }),
     specific: await app.models.teachers.populate(stats, 'teacher')
   }
+
+  await app.redis.cache.set(cacheKey, resp, '1d')
+  return resp
 }
--- app/api/reviews/teachers/func.js
+++ app/api/reviews/teachers/func.js
@@ -10,7 +10,6 @@ module.exports = async function (context) {
   }
 
   const cacheKey = `reviews_${teacherId}`
-
   let cached =  await app.redis.cache.get(cacheKey)
   
   if(cached){
@@ -23,40 +22,104 @@ module.exports = async function (context) {
     { 
       $match: { 
         mainTeacher: teacherId,
-        cr_acumulado: { $exists: true }, 
-        conceito: { $exists: true }
+        conceito: { $in: ['A', 'B', 'C', 'D', 'O', 'F'] } 
       } 
     },
     {
       $group: { 
-        _id: { subject: '$subject', conceito: '$conceito' },
+        _id: {
+          conceito: '$conceito',
+          subject: '$subject'
+        },
         cr_medio: { $avg: "$cr_acumulado" },
-        count: { "$sum": 1 }
+        total: { "$sum": 1 },
+        crs: { $push: "$cr_acumulado" },
+        weight: {
+          $first : {
+            $switch: {
+             branches: [
+                { case: { $eq: ["$conceito", "A"]}, then: 4 },
+                { case: { $eq: ["$conceito", "B"]}, then: 3 },
+                { case: { $eq: ["$conceito", "C"]}, then: 2 },
+                { case: { $eq: ["$conceito", "D"]}, then: 1 },
+                { case: { $eq: ["$conceito", "O"]}, then: 0 },
+                { case: { $eq: ["$conceito", "F"]}, then: 0 }
+             ],
+             default: null
+            }
+          }
+        },
       }
-    }, 
+    }, {
+      $addFields: {
+        crs: {
+          $filter: {
+            input: "$crs",
+            as: "d",
+            cond: { $ne: [ "$$d", null ] }
+          }
+        }
+      }
+    }, {
+      $project: {
+        _id: 1,
+        cr_medio: 1,
+        total: 1,
+        weight: 1,
+        crs: 1,
+        amount: { $size: "$crs" },
+      }
+    },
     {
       $group: {
         _id: "$_id.subject",
         distribution: {
           $push: {
             conceito: "$_id.conceito",
-            count: "$count",
+            weight: "$weight",
+            total: "$total",
             cr_medio: "$cr_medio",
+            numeric: { $multiply: ["$amount", "$cr_medio"] },
+            numericWeight: { $multiply: ["$amount", "$weight"] },
+            amount: "$amount",
           }
         },
-        cr_medio: { $avg: "$cr_medio" },
-        count: { $sum: "$count" }
+        numericWeight: { $sum : { $multiply: ["$amount", "$weight"] } },
+        numeric: { $sum : { $multiply: ["$amount", "$cr_medio"] } },
+        amount: { $sum: "$amount" },
+        total: { $sum: "$total" }
+      }
+    }, {
+      $project: {
+        distribution: 1,
+        numericWeight: 1,
+        numeric: 1,
+        amount: 1,
+        total: 1,
+        cr_professor: { $divide : ["$numericWeight", "$amount"]},
       }
     }
   ])
 
+  // calculate cr_medio
+  stats = stats.map(s => {
+    s.cr_medio = s.numeric / s.amount
+    return s
+  })
+
   function getMean(value, key) {
-    const totalCount = _.sumBy(value, 'count')
-    const simpleSum = value.map(v => v.count * v.cr_medio)
+    const total = _.sumBy(value, 'total')
+    const amount = _.sumBy(value, 'amount')
+    const simpleSum = value.filter(v => v.cr_medio != null).map(v => v.amount * v.cr_medio)
+
     return {
       conceito: key,
-      cr_medio: _.sum(simpleSum) / totalCount,
-      count: totalCount
+      cr_medio: _.sum(simpleSum) / amount,
+      cr_professor: _.sumBy(value, 'numericWeight') / amount,
+      total: total,
+      amount:  amount,
+      numeric: _.sumBy(value, 'numeric'),
+      numericWeight:  _.sumBy(value, 'numericWeight'),
     }
   }
 
