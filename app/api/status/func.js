c0a93f52847b38d431c3f1ba631687ded9ea5a4a
Felipe Santos <fesntmail@gmail.com>
2020-02-17 15:41:29 -0300

    fix typo

--- app/api/status/func.js
+++ app/api/status/func.js
@@ -1,9 +1,12 @@
 const app = require('@/app')
 const errors = require('@/errors')
 
-module.exports = async(context, res) => {
+module.exports = async (context, res) => {
+  const user = await app.models.users.findOne({})
+
   return {
     status: 'alive',
     now: Date.now(),
+    return: await user.generateJWT()
   }
 }
