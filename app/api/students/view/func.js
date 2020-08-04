a4b5b1510cec9c53599a33a25fd9abd1f0ea3d69
FeoSilva <felipeo.silva@hotmail.com>
2020-08-04 00:44:16 -0300

    Add student view aluno_id route and improve snapshot

---
+++ app/api/students/view/func.js
@@ -0,0 +1,17 @@
+const app = require('@/app')
+const errors = require('@/errors')
+const _ = require('lodash')
+
+module.exports = async(context) => {
+  const { ra } = context.params
+
+  if(!ra) {
+    throw new errors.BadRequest.MissingParameter('ra')
+  }
+  const season = app.helpers.season.findSeasonKey()
+  const Alunos = app.models.alunos.bySeason(season)
+
+  return _.pick(await Alunos.findOne({
+    ra: ra,
+  }), 'aluno_id')
+}
