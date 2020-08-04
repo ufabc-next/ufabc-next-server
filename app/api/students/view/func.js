8c734563aedeb616affc582f532e27b53c5fc81c
FeoSilva <felipeo.silva@hotmail.com>
2020-08-04 15:13:37 -0300

    Add authentication to snapshot

--- app/api/students/view/func.js
+++ app/api/students/view/func.js
@@ -3,21 +3,14 @@ const errors = require('@/errors')
 const _ = require('lodash')
 
 module.exports = async(context) => {
-  const { ra } = context.params
-  const { login } = context.query
-
+  const { ra } = context.user.ra
   if(!ra) {
     throw new errors.BadRequest.MissingParameter('ra')
-  }
-
-  if(!login) {
-    throw new errors.BadRequest.MissingParameter('login')
   }
   const season = app.helpers.season.findSeasonKey()
   const Alunos = app.models.alunos.bySeason(season)
 
   return _.pick(await Alunos.findOne({
     ra: ra,
-    login: login
   }), 'aluno_id')
 }
