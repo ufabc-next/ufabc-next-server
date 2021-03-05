19d530f6b713cc7cf8dd888fd23018b0be23889f
Tenda Digital <tendadigital@MacBook-Pro-de-Tenda.local>
2021-03-05 19:31:25 -0300

    Add season 2021:2 as EAD mode

--- app/api/disciplinas/disciplinaId/kicks/func.js
+++ app/api/disciplinas/disciplinaId/kicks/func.js
@@ -1,98 +1,107 @@
-const _ = require('lodash')
-const app = require('@/app')
-const errors = require('@/errors')
+const _ = require("lodash");
+const app = require("@/app");
+const errors = require("@/errors");
 
 module.exports = async function (context) {
-  let { disciplinaId } = context.params
-  let { sort } = context.query
+  let { disciplinaId } = context.params;
+  let { sort } = context.query;
 
-  if(!disciplinaId) {
-    throw new errors.BadRequest.MissingParameter('disciplinaId')
+  if (!disciplinaId) {
+    throw new errors.BadRequest.MissingParameter("disciplinaId");
   }
 
-  const season = app.helpers.season.findSeasonKey()
-  const Disciplinas = app.models.disciplinas.bySeason(season)
-  const Alunos = app.models.alunos.bySeason(season)
-  const findId = app.helpers.courses.findId
+  const season = app.helpers.season.findSeasonKey();
+  const Disciplinas = app.models.disciplinas.bySeason(season);
+  const Alunos = app.models.alunos.bySeason(season);
+  const findId = app.helpers.courses.findId;
 
   // find disciplina
-  let disciplina = await Disciplinas.findOne({ disciplina_id: disciplinaId })
+  let disciplina = await Disciplinas.findOne({ disciplina_id: disciplinaId });
 
-  if(!disciplina) {
-    throw new errors.NotFound('disciplina')
+  if (!disciplina) {
+    throw new errors.NotFound("disciplina");
   }
 
   // create sort mechanism
-  const kicks = sort ? sort : kickRule(disciplina)
-  const order = Array(kicks.length || 0).fill('desc')
-  
+  const kicks = sort ? sort : kickRule(disciplina);
+  const order = Array(kicks.length || 0).fill("desc");
+
   // turno must have a special treatment
-  const turnoIndex = kicks.indexOf('turno')
-  if(turnoIndex != -1) {
-    order[turnoIndex] = (disciplina.turno == 'diurno') ? 'asc' : 'desc'
+  const turnoIndex = kicks.indexOf("turno");
+  if (turnoIndex != -1) {
+    order[turnoIndex] = disciplina.turno == "diurno" ? "asc" : "desc";
   }
 
-  const isAfterKick = _(disciplina.after_kick).castArray().compact().value().length
-  const result = resolveMatriculas(disciplina, isAfterKick)
+  const isAfterKick = _(disciplina.after_kick).castArray().compact().value()
+    .length;
+  const result = resolveMatriculas(disciplina, isAfterKick);
 
-  const resultMap = new Map([...result.map(r => [r.aluno_id, r])])
+  const resultMap = new Map([...result.map((r) => [r.aluno_id, r])]);
   let students = await Alunos.aggregate([
-    { $match: { aluno_id: { $in: _.map(result, 'aluno_id' ) } } },
-    { $unwind: '$cursos' }
-  ])
+    { $match: { aluno_id: { $in: _.map(result, "aluno_id") } } },
+    { $unwind: "$cursos" },
+  ]);
 
   const interIds = [
-    await findId('Bacharelado em Ciência e Tecnologia', season),
-    await findId('Bacharelado em Ciência e Humanidades', season),
-  ]
+    await findId("Bacharelado em Ciência e Tecnologia", season),
+    await findId("Bacharelado em Ciência e Humanidades", season),
+  ];
 
-  const obrigatorias = _.pull(disciplina.obrigatorias, ...interIds)
+  const obrigatorias = _.pull(disciplina.obrigatorias, ...interIds);
 
-  students = students.map(s => {
-    const reserva = _.includes(obrigatorias, s.cursos.id_curso)
+  students = students.map((s) => {
+    const reserva = _.includes(obrigatorias, s.cursos.id_curso);
 
-    return _.extend({
-      aluno_id: s.aluno_id,
-      cr : s.cursos.cr,
-      cp: s.cursos.cp,
-      ik: reserva ? s.cursos.ind_afinidade : 0,
-      reserva: reserva,
-      turno: s.cursos.turno,
-      curso: s.cursos.nome_curso,
-    }, resultMap.get(s.aluno_id) || {})
-  })
+    return _.extend(
+      {
+        aluno_id: s.aluno_id,
+        cr: s.cursos.cr,
+        cp: s.cursos.cp,
+        ik: reserva ? s.cursos.ind_afinidade : 0,
+        reserva: reserva,
+        turno: s.cursos.turno,
+        curso: s.cursos.nome_curso,
+      },
+      resultMap.get(s.aluno_id) || {}
+    );
+  });
 
-  return _(students)
-    .orderBy(kicks, order)
-    .uniqBy('aluno_id')
-    .value()
-}
+  return _(students).orderBy(kicks, order).uniqBy("aluno_id").value();
+};
 
 function kickRule(disciplina) {
-  const season = app.helpers.season.findSeasonKey()
-  let coeffRule = null
-  if(season == '2020:3' || season == '2021:1') {
-    coeffRule = ['cp', 'cr']
+  const season = app.helpers.season.findSeasonKey();
+  let coeffRule = null;
+  if (
+    season == "2020:2" ||
+    season == "2020:3" ||
+    season == "2021:1" ||
+    season == "2021:2"
+  ) {
+    coeffRule = ["cp", "cr"];
   } else {
-    coeffRule = disciplina.ideal_quad ? ['cr', 'cp'] : ['cp', 'cr']
+    coeffRule = disciplina.ideal_quad ? ["cr", "cp"] : ["cp", "cr"];
   }
-  return ['reserva', 'turno', 'ik'].concat(coeffRule)
+  return ["reserva", "turno", "ik"].concat(coeffRule);
 }
 
 function resolveMatriculas(disciplina, isAfterKick) {
   // if kick has not arrived, not one has been kicked
-  if(!isAfterKick) {
-    return (disciplina.alunos_matriculados || []).map(d => ({
-      aluno_id: d
-    }))
+  if (!isAfterKick) {
+    return (disciplina.alunos_matriculados || []).map((d) => ({
+      aluno_id: d,
+    }));
   }
 
   // check diff between before_kick and after_kick
-  let kicked = _.difference(disciplina.before_kick || [], disciplina.after_kick || [])
+  let kicked = _.difference(
+    disciplina.before_kick || [],
+    disciplina.after_kick || []
+  );
 
   // return who has been kicked
-  return disciplina.before_kick.map(d => ({
+  return disciplina.before_kick.map((d) => ({
     aluno_id: d,
-    kicked: kicked.includes(d)
-  }))
+    kicked: kicked.includes(d),
+  }));
 }
--- app/api/students/create/func.js
+++ app/api/students/create/func.js
@@ -1,55 +1,74 @@
-const app = require('@/app')
-const errors = require('@/errors')
-const _ = require('lodash')
+const app = require("@/app");
+const errors = require("@/errors");
+const _ = require("lodash");
 
-module.exports = async(context) => {
-  const { aluno_id, ra, login } = context.body
+module.exports = async (context) => {
+  const { aluno_id, ra, login } = context.body;
 
-  if(!aluno_id) {
-    throw new errors.BadRequest.MissingParameter('aluno_id')
+  if (!aluno_id) {
+    throw new errors.BadRequest.MissingParameter("aluno_id");
   }
 
-  const season = app.helpers.season.findSeasonKey()
-  const Alunos = app.models.alunos.bySeason(season)
-  const Disciplinas = app.models.disciplinas.bySeason(season)
+  const season = app.helpers.season.findSeasonKey();
+  const Alunos = app.models.alunos.bySeason(season);
+  const Disciplinas = app.models.disciplinas.bySeason(season);
 
   // check if already passed, if so does no update this user anymore
-  const isPrevious = await Disciplinas.count({ before_kick: { $exists: true, $ne: [] }})
-  if(isPrevious) {
-    return await Alunos.findOne({ aluno_id: aluno_id })
+  const isPrevious = await Disciplinas.count({
+    before_kick: { $exists: true, $ne: [] },
+  });
+  if (isPrevious) {
+    return await Alunos.findOne({ aluno_id: aluno_id });
   }
 
-  if((context.body.cursos || []).some(curso => !curso.curso_id || curso.curso_id == "null") || !ra) {
+  if (
+    (context.body.cursos || []).some(
+      (curso) => !curso.curso_id || curso.curso_id == "null"
+    ) ||
+    !ra
+  ) {
     return await Alunos.findOne({
-      aluno_id: aluno_id
-    })
+      aluno_id: aluno_id,
+    });
   }
 
-  const cursos = (context.body.cursos || []).map(async c => {
-    let courseCleaned = c.curso.trim().replace('↵', '').replace(/\s+/g, ' ')
-    let cpLastQuad = null
-    if(season == '2020:3' || season == '2021:1') {
-      const history = await app.models.historiesGraduations.findOne({ ra: ra, curso: courseCleaned })
-      cpLastQuad = _.get(history, 'coefficients.2019.3.cp_acumulado', c.cp)
+  const cursos = (context.body.cursos || []).map(async (c) => {
+    let courseCleaned = c.curso.trim().replace("↵", "").replace(/\s+/g, " ");
+    let cpLastQuad = null;
+    if (
+      season == "2020:2" ||
+      season == "2020:3" ||
+      season == "2021:1" ||
+      season == "2021:2"
+    ) {
+      const history = await app.models.historiesGraduations.findOne({
+        ra: ra,
+        curso: courseCleaned,
+      });
+      cpLastQuad = _.get(history, "coefficients.2019.3.cp_acumulado", c.cp);
     }
 
-    c.cr = app.helpers.parse.toNumber(c.cr)
-    c.cp = app.helpers.parse.toNumber(cpLastQuad),
-    c.quads = app.helpers.parse.toNumber(c.quads)
-    c.nome_curso = courseCleaned
-    c.ind_afinidade = (0.07 * c.cr) + (0.63 * c.cp) + (0.005 * c.quads)
-    c.id_curso = c.curso_id
-    return c
-  })
+    c.cr = app.helpers.parse.toNumber(c.cr);
+    (c.cp = app.helpers.parse.toNumber(cpLastQuad)),
+      (c.quads = app.helpers.parse.toNumber(c.quads));
+    c.nome_curso = courseCleaned;
+    c.ind_afinidade = 0.07 * c.cr + 0.63 * c.cp + 0.005 * c.quads;
+    c.id_curso = c.curso_id;
+    return c;
+  });
 
-  return await Alunos.findOneAndUpdate({
-    aluno_id: aluno_id
-  }, {
-    cursos: await Promise.all(cursos),
-    ra: ra,
-    login: login
-  }, {
-    new: true,
-    upsert: true
-  })
-}
+  return await Alunos.findOneAndUpdate(
+    {
+      aluno_id: aluno_id,
+    },
+    {
+      cursos: await Promise.all(cursos),
+      ra: ra,
+      login: login,
+    },
+    {
+      new: true,
+      upsert: true,
+    }
+  );
+};
