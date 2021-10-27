b40923d0f60e36e7139b5ce8f0b3ec3804cc7d4d
Felipe Tiozo <felipetiozo10@gmail.com>
2021-10-27 20:55:45 -0300

    Fix and skip broken tests

--- app/api/disciplinas/sync/spec.js
+++ app/api/disciplinas/sync/spec.js
@@ -1,36 +1,44 @@
-const app = require('@/app')
-const assert = require('assert')
-const populate = require('@/populate')
-const sinon = require('sinon')
-const Axios = require('axios')
+const app = require("@/app");
+const assert = require("assert");
+const populate = require("@/populate");
+const sinon = require("sinon");
+const Axios = require("axios");
 
-const func = require('./func')
+const func = require("./func");
 
-describe('POST /v1/disciplinas/sync', function() {
+describe("POST /v1/disciplinas/sync", function () {
   beforeEach(async function () {
-    await populate({ operation : 'both', only: ['disciplinas'] })
-  })
+    await populate({ operation: "both", only: ["disciplinas"] });
+  });
 
-  describe('func', function () {
-    it('sync disciplines', async function () {
-      let file = app.helpers.test.getDisciplinas()
-      file.data = app.helpers.test.sample(file.data, 200)
-      let stub = sinon.stub(Axios, 'get').returns(file)
+  describe("func", function () {
+    it("sync disciplines", async function () {
+      let file = app.helpers.test.getDisciplinas();
+      file.data = app.helpers.test.sample(file.data, 200);
 
-      await func()
+      let axiosInstanceStub;
+      let axiosGetStub;
+      try {
+        axiosInstanceStub = sinon.stub(Axios, "create").returns(Axios);
+        axiosGetStub = sinon.stub(Axios, "get").returns(file);
+        await func();
 
-      // check if is scoped by season
-      let Disciplinas = app.models.disciplinas.bySeason('2018:2')
-      assert.equal(await Disciplinas.findOne({}), null)
+        // check if is scoped by season
+        let Disciplinas = app.models.disciplinas.bySeason("2018:2");
+        assert.equal(await Disciplinas.findOne({}), null);
 
-      const season = app.helpers.season.findSeasonKey()
-      Disciplinas = app.models.disciplinas.bySeason(season)
+        const season = app.helpers.season.findSeasonKey();
+        Disciplinas = app.models.disciplinas.bySeason(season);
 
-      assert.equal(await Disciplinas.countDocuments({}), 200)
-      let disciplina = await Disciplinas.findOne({ disciplina_id : 2538 }).lean(true)
-      assert.equal(disciplina.disciplina_id, 2538)
-
-      stub.restore()
-    })
-  })
-})
+        assert.equal(await Disciplinas.countDocuments({}), 200);
+        let disciplina = await Disciplinas.findOne({
+          disciplina_id: 2538,
+        }).lean(true);
+        assert.equal(disciplina.disciplina_id, 2538);
+      } finally {
+        axiosInstanceStub.restore();
+        axiosGetStub.restore();
+      }
+    });
+  });
+});
