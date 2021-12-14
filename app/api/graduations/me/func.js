4192ad8f8138c8024c8048e976a094d937769433
Egidio Henrique Paixão <egidio-henrique@hotmail.com>
2021-12-14 17:13:39 -0300

    Update app/api/graduations/me/func.js
    Co-authored-by: Felipe Augusto <fesntmail@gmail.com>

--- app/api/graduations/me/func.js
+++ app/api/graduations/me/func.js
@@ -1,7 +1,7 @@
 const app = require('@/app')
 
 module.exports = async function (context) {
-  const historiesGraduationIds = await app.models.historiesGraduations.find({ ra: context.user.ra}).select("_id")
+  const historiesGraduationIds = await app.models.historiesGraduations.find({ ra: context.user.ra}).select('_id')
   const intendedGraduationIds = await app.models.intendedGraduation.find({ user: context.user._id, active: true }).select("_id")
 
   const result = [...historiesGraduationIds,...intendedGraduationIds]
