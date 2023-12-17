4300b39153752c51f8a7245bc171270920add8dd
brMonteiro-G <brgabriel.monteiro@gmail.com>
2023-12-17 02:35:43 -0300

    chore: remove git statement from status endpoint

--- app/api/status/func.js
+++ app/api/status/func.js
@@ -2,8 +2,6 @@ module.exports = async () => {
   return {
     status: 'alive',
     now: Date.now(),
-    hash: require('child_process')
-      .execSync('git rev-parse HEAD')
-      .toString().trim()
+    hash: Math.random()
   }
 }
