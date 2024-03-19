6e1bad7c2e8bf095a4ca90df91132c00f56dc37e
André Sakuma <andre.yuji.sakuma@gmail.com>
2024-03-18 23:24:45 -0300

    add graduation stats feature

---
+++ app/api/graduations/list/func.js
@@ -0,0 +1,49 @@
+const app = require('@/app')
+
+module.exports = async function func(context) {
+  const Graduation = await app.models.graduation
+  const graduations = await Graduation.find({}).lean()
+
+  const Subject = await app.models.subjects
+  const SubjectGraduations = await app.models.subjectGraduations
+
+  const populatedGraduations = await Promise.all(
+    graduations.map(async (graduation) => {
+      const subjectsGraduations = await SubjectGraduations.find({
+        graduation: graduation._id,
+      }).lean()
+
+      const subjects = (
+        await Promise.all(
+          subjectsGraduations.map(async (subjectGraduation) => {
+            const subject = await Subject.findOne({
+              _id: subjectGraduation.subject,
+            }).lean()
+            return {
+              ...subjectGraduation,
+              subject,
+            }
+          })
+        )
+      ).reduce((acc, subject) => {
+        const category = subject.category
+        if (!category) {
+          console.log('no category', subject)
+          return acc
+        }
+        if (!acc[category]) {
+          acc[category] = []
+        }
+        acc[category].push(subject)
+        return acc
+      }, {})
+
+      return {
+        ...graduation,
+        subjects,
+      }
+    })
+  )
+
+  return populatedGraduations
+}
